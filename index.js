594537d22e7d4a1dd7931a4b41c749b86adf0efe
fwakks <70325823+fwakks@users.noreply.github.com>
2021-03-18 19:21:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -392,7 +392,7 @@ function process_commands_query(query, mapKey, userid) {
 
     let out = null;
 
-    const regex = /^chat ([a-zA-Z]+)(.+?)?$/;
+    const regex = /^bobby ([a-zA-Z]+)(.+?)?$/;
     const m = query.toLowerCase().match(regex);
     if (m && m.length) {
         const cmd = (m[1]||'').trim();
